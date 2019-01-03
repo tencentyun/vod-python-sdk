d35944169733363d2b7b604945e6b92d802edcdd
xujianguo <450846733@qq.com>
2019-01-03 21:05:45 +0800

    up version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def long_description():
 
 setup(
     name='vod-python-sdk',
-    version='1.2.0',
+    version='1.3.0',
     url='https://github.com/tencentyun/vod-python-sdk',
     license='MIT',
     author='vod',
